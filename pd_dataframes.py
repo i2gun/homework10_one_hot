223a02ce3acd22a80a964f2708387c1e53efee4f
i2gun <i2gun@bk.ru>
2023-12-16 21:53:32 +0600

    Adding initial conditions

--- pd_dataframes.py
+++ pd_dataframes.py
@@ -1,3 +1,10 @@
 # Задача 44: В ячейке ниже представлен код генерирующий DataFrame,
 # который состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
 
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+df = pd.DataFrame({'whoAmI':lst})
