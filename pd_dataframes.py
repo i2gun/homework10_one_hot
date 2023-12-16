b162d0ec8509507458c18034babd9c53553d6ebb
i2gun <i2gun@bk.ru>
2023-12-16 21:59:17 +0600

    Committing the file with the solution

--- pd_dataframes.py
+++ pd_dataframes.py
@@ -8,3 +8,16 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 df = pd.DataFrame({'whoAmI':lst})
+
+#----------  Решение ---------------------------
+
+df['whoAmI_human'] = 0
+df['whoAmI_robot'] = 0
+df.loc[df['whoAmI'] == 'human', 'whoAmI_human'] = 1
+df.loc[df['whoAmI'] == 'robot', 'whoAmI_robot'] = 1
+df = df.drop(columns='whoAmI')
+
+# вывод аналогичен выводу функции get_dummies():
+#      df = pd.get_dummies(df);
+
+df
